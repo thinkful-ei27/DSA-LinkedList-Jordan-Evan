a427753b58278c6cfc7bfa3291d69fe1e993eb8f
Jordan Heffernan <jheff2@gmail.com>
2019-02-28 09:40:21 -0800

    tested inserts and remove

--- drills.js
+++ drills.js
@@ -5,10 +5,13 @@ function main() {
   SSL.insertFirst('Apollo');
   SSL.insertLast('Boomer');
   SSL.insertLast('Helo');
-
   SSL.insertLast('Husker');
   SSL.insertLast('Starbuck');
+  SSL.insertLast('Tauhida');
   console.log(JSON.stringify(SSL));
+  SSL.remove('Tauhida');
+  console.log(JSON.stringify(SSL));
+  SSL.remove('squirrel');
   return SSL;
 }
 main();
