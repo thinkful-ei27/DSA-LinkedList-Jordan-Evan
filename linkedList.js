7d880478923f113988f29b388b07095e3fb7b840
Jordan Heffernan <jheff2@gmail.com>
2019-02-28 08:53:35 -0800

    insertFirst and insertLast

---
+++ linkedList.js
@@ -0,0 +1,27 @@
+const _Node = require('./node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
+    }
+  }
+
+
+}
+
+module.exports = LinkedList;
